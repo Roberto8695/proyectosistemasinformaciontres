1a1eb71647048d971168da1251dabf698dff8094
Otrebor19 <96426304+Otrebor19@users.noreply.github.com>
2025-03-31 14:54:00 -0400

    add

--- frontend/next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {  output: "export",
-    basePath: "",
-    assetPrefix: "https://losmiserablesproyectosupds.me/",
-    trailingSlash: true,};
-
-export default nextConfig;
